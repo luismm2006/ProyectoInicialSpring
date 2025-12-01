79a918ec753c9ff6ef488923cdee34e2730408e2
luismm2006 <luismulamarquez@gmail.com>
2025-12-01 12:40:41 +0100

    Creación de inicio de proyecto

---
+++ src/test/java/com/Luis/ProyectoInicialSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Luis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoInicialSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
